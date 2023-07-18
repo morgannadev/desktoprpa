a49a5f9d66ea3c0f7bc0e9d980adeedf9aac2625
morgannadev <morgcontato@gmail.com>
2023-07-18 15:48:40 +0000

    build the desktop bot with sicalc

--- DesktopRPA/bot.py
+++ DesktopRPA/bot.py
@@ -1,34 +1,103 @@
-# Import for the Desktop Bot
 from botcity.core import DesktopBot
-
-# Import for integration with BotCity Maestro SDK
 from botcity.maestro import *
+
+# import random
 
 # Disable errors if we are not connected to Maestro
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
 
 def main():
-    # Runner passes the server url, the id of the task being executed,
-    # the access token and the parameters that this task receives (when applicable).
-    maestro = BotMaestroSDK.from_sys_args()
-    ## Fetch the BotExecution with details from the task, including parameters
-    execution = maestro.get_execution()
-
-    print(f"Task ID is: {execution.task_id}")
-    print(f"Task Parameters are: {execution.parameters}")
 
     bot = DesktopBot()
-    bot.browse("http://www.botcity.dev")
+    
+    # Precisamos primeiro abrir o SiCalc
+    bot.execute(r"C:\Program Files (x86)\Programas RFB\Sicalc Auto Atendimento\SicalcAA.exe")
+    
+    # Em seguida, precisamos fechar a janela de esclarecimento
+    if not bot.find( "janela_esclarecimento", matching=0.97, waiting_time=10000):
+        not_found("janela_esclarecimento")
+    bot.click_relative(221, 206) 
+    
+    # Identificando o menu funções para abrir o preenchimento da DARF
+    if not bot.find( "menu_funcoes", matching=0.97, waiting_time=10000):
+        not_found("menu_funcoes")
+    bot.click()
+    
+    if not bot.find( "menu_preenchimento_darf", matching=0.97, waiting_time=10000):
+        not_found("menu_preenchimento_darf")
+    bot.click()
+    
+    if not bot.find( "item_codigo_receita", matching=0.97, waiting_time=10000):
+        not_found("item_codigo_receita")
+    bot.click_relative(149, 9)
 
-    # Implement here your logic...
-    ...
+    # Neste momento, estamos no campo Cod Receita e vamos preencher com o valor
+    bot.paste("5629")
 
-    # Uncomment to mark this task as finished on BotMaestro
-    # maestro.finish_task(
-    #     task_id=execution.task_id,
-    #     status=AutomationTaskFinishStatus.SUCCESS,
-    #     message="Task Finished OK."
-    # )
+    # Precisamos pressionar tab para avançar
+    bot.tab()
+
+    # às vezes essa parte dá erro, escolhi deixar um tempo para poder trocar a tela
+    bot.wait(1000)
+    
+    if not bot.find( "item_periodo_apuracao", matching=0.97, waiting_time=10000):
+        not_found("item_periodo_apuracao")
+    bot.click_relative(12, 22)
+    bot.paste("040121")
+
+    if not bot.find( "item_valor_reais", matching=0.97, waiting_time=10000):
+        not_found("item_valor_reais")
+    bot.click_relative(18, 26)
+    bot.paste("1525")
+    
+    if not bot.find( "botao_calcular", matching=0.97, waiting_time=10000):
+        not_found("botao_calcular")
+    bot.click()
+    
+    if not bot.find( "botao_darf", matching=0.97, waiting_time=10000):
+        not_found("botao_darf")
+    bot.click()
+    
+    if not bot.find( "item_nome", matching=0.97, waiting_time=10000):
+        not_found("item_nome")
+    bot.click_relative(22, 26)
+    bot.paste("Petrobras")
+    
+    if not bot.find( "item_telefone", matching=0.97, waiting_time=10000):
+        not_found("item_telefone")
+    bot.click_relative(13, 29)
+    bot.paste("1199991234")
+    
+    if not bot.find( "item_cnpj", matching=0.97, waiting_time=10000):
+        not_found("item_cnpj")
+    bot.click_relative(131, 10)
+    bot.paste("33000167000101")
+    
+    if not bot.find( "item_referencia", matching=0.97, waiting_time=10000):
+        not_found("item_referencia")
+    bot.click_relative(134, 11)
+    bot.paste("0")
+
+    if not bot.find( "botao_imprimir", matching=0.97, waiting_time=10000):
+        not_found("botao_imprimir")
+    bot.click()
+    
+    if not bot.find( "item_arquivo", matching=0.97, waiting_time=10000):
+        not_found("item_arquivo")
+    bot.click_relative(74, 2)
+    # valor = random.randint(0, 9999)
+    bot.paste(r"C:\temp\darfs\NF.pdf")
+    
+    if not bot.find( "botao_salvar", matching=0.97, waiting_time=10000):
+        not_found("botao_salvar")
+    bot.click()  
+
+    bot.wait(1000)
+
+    # Aqui fechamos a primeira janela e depois fechamos o sicalc
+    bot.alt_f4()
+    bot.alt_f4()
+    
 
 def not_found(label):
     print(f"Element not found: {label}")
@@ -36,3 +105,4 @@ def not_found(label):
 
 if __name__ == '__main__':
     main()
+
